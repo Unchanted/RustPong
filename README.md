079d0f96603dd5ba22f025ca2208d064fd173498
Unchanted <omkkdwivedi@gmail.com>
2024-07-22 21:12:10 +0530

    multiple changes

--- README.md
+++
@@ -1,1 +0,0 @@
-# Arcade29-RustPong
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use crate::view::*;
 
 use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::EventPump;
 use sdl2::keyboard::Keycode;
 
 mod entity;
@@ -18,6 +19,10 @@ const RACKET_WIDTH: u32 = 10;
 const RACKET_HEIGHT: u32 = WINDOW_HEIGHT / 10;
 
 const SCREEN_MARGIN: i32 = 10;
+
+const FRAME_DURATION: u32 = 50;
+
+struct FrameEvent;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -36,26 +41,62 @@ pub fn main() {
 
 fn game_loop(context: &sdl2::Sdl,
              canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
-    canvas.set_draw_color(Color::BLACK);
-    canvas.clear();
 
     let mut gs: GameState = initialize_game_state();
-
     let mut event_pump = context.event_pump().unwrap();
+    let ev = context.event().unwrap();
+    ev.register_custom_event::<FrameEvent>().unwrap();
+
+    let timer_subsystem = context.timer().unwrap();
+    let _timer = timer_subsystem.add_timer(
+        FRAME_DURATION,
+        Box::new(|| {
+            ev.push_custom_event(FrameEvent).unwrap();
+            FRAME_DURATION
+        }),
+    );
     while !gs.is_game_over {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    gs.is_game_over = true;
-                },
-                _ => {}
-            }
-        }
+        handle_game_events(&mut gs, &mut event_pump);
+
         gs.ball.update_position();
-        draw_ball(&gs.ball, canvas);
-        draw_racket(&gs.racket_1, canvas);
-        draw_racket(&gs.racket_2, canvas);
+
+        if gs.ball.has_collision_with(&gs.racket_1) {
+            println!("ball: {}", gs.ball.to_string());
+            //println!("collision angle {}", gs.ball.get_angle_from_collision_with(&gs.racket_1));
+            gs.ball.inverse_direction();
+        }
+
+        if gs.ball.has_collision_with(&gs.racket_2) {
+            //println!("collision angle {}", gs.ball.get_angle_from_collision_with(&gs.racket_2));
+            println!("ball: {}", gs.ball.to_string());
+            gs.ball.inverse_direction();
+        }
+
+        // draw the game
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+        draw_game(&gs, canvas);
         canvas.present();
+    }
+}
+
+fn handle_game_events(gs: &mut GameState, event_pump: &mut EventPump){
+    let event = event_pump.wait_event();
+    if event.is_user_event() {
+        //
+    }else {
+        match event {
+            Event::Quit {..} |
+            Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                gs.is_game_over = true;
+            },
+            Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
+                gs.racket_1.move_up();
+            },
+            Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
+                gs.racket_1.move_down();
+            },
+            _ => {}
+        }
     }
 }
